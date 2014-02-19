8303b7b5c84b35ea109fd46e07a1b89e5d8d33c3
Matt Duffy <mttdffy@gmail.com>
2014-02-19 15:00:28 -0500

    Task 2 - Adds validations to book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ActiveRecord::Base
+
+  validates :title,       presence: true
+  validates :author,      presence: true
+  validates :summary,     presence: true
+  validates :isbn,        presence: true,
+                          length: {is: 10}
 end
